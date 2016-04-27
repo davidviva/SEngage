4932ff8d81101de05bd6d19253b00a1ccef0872e
davidviva <wooyanmis@gmail.com>
2016-04-27 10:11:12 -0400

    3.6.3.1
    Me part

--- SEngage/SnapE/Me/MeTableViewController.swift
+++ SEngage/SnapE/Me/MeTableViewController.swift
@@ -2,26 +2,41 @@
 //  MeTableViewController.swift
 //  SEngage
 //
-//  Created by Yan Wu on 3/29/16.
+//  Created by Yan Wu on 4/27/16.
 //  Copyright © 2016 Avaya. All rights reserved.
 //
 
 import UIKit
 
 class MeTableViewController: UITableViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var user: UserObj!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "My Profile"
         
-        // 去掉尾部多余行？？？
-        self.tableView.tableFooterView = UIView(frame:CGRectZero)
-        
+        user = GenerateData.generateUser()
+        photoImageView.image = user.photo
+        nameLabel.text = user.name
+        phoneLabel.text = user.phone
+        emailLabel.text = user.email
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "changePhoto" {
+            let photoViewController = segue.destinationViewController as! PhotoViewController
 
+            photoViewController.user = user
+        }
+    }
 }
--- SEngage/SnapE/Me/PhotoViewController.swift
+++ SEngage/SnapE/Me/PhotoViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class PhotoViewController: SEViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    var user: UserObj?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,5 +67,23 @@ class PhotoViewController: SEViewController, UIImagePickerControllerDelegate, UI
         alertController.addAction(pickPhotoAction)
         alertController.addAction(cancelAction)
         self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        imageView.image = selectedImage
+        user?.photo = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
