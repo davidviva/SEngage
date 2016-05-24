dfacac844702963f50aea02da99de67b0640d013
davidviva <wooyanmis@gmail.com>
2016-05-24 15:13:42 -0400

    3.13.3
    team creation searchbar and text field refreshed

--- SEngage/SnapE/Contacts/TeamCreationViewController.swift
+++ SEngage/SnapE/Contacts/TeamCreationViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class TeamCreationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+class TeamCreationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, UISearchBarDelegate {
 
     // MARK: Properties
     var group: Group?
     var contacts = [Contact]()
+    var searchContacts = [Contact]()
     var groupContacts = [Contact]()
     var selectedData = [Int]()
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +26,18 @@ class TeamCreationViewController: UIViewController, UITableViewDelegate, UITable
         
         loadContacts()
         initTextField()
+        self.searchBar.delegate = self
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        
+        // Register the gesture for dismissing the keyboard
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(TeamCreationViewController.handleTap(_:))))
     }
     
     func loadContacts() {
         contacts = GenerateData.generateContacts(10)
         contacts.sortInPlace({$0.name < $1.name})
+        searchContacts = contacts
     }
     
     func initTextField() {
@@ -54,7 +61,7 @@ class TeamCreationViewController: UIViewController, UITableViewDelegate, UITable
     
     // set rows in section
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        return searchContacts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -66,7 +73,7 @@ class TeamCreationViewController: UIViewController, UITableViewDelegate, UITable
         cell.selectionStyle = UITableViewCellSelectionStyle.None
         
         // Fetches the appropriate contact for the data source layout.
-        let contact = contacts[indexPath.row]
+        let contact = searchContacts[indexPath.row]
         
         cell.nameLabel.text = contact.name
         cell.photoImageView.image = contact.photo
@@ -92,10 +99,68 @@ class TeamCreationViewController: UIViewController, UITableViewDelegate, UITable
         self.selectedData.removeAtIndex(index!)
     }
     
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            textField.resignFirstResponder()
+            searchBar.resignFirstResponder()
+        }
+        sender.cancelsTouchesInView = false
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        textField.resignFirstResponder()
+    }
+    
+    // Hide the keyboard when click ‘return’
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    // *Search Result
+    
+    // Method of UISearchBarDelegate，called when the search text changed
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // show all the cell when there's no input
+        if searchText == "" {
+            self.searchContacts = self.contacts
+        }
+        else { // Match the input regardless of the Uppercase
+            self.searchContacts = []
+            for contact in self.contacts {
+                if contact.name.lowercaseString.hasPrefix(searchText.lowercaseString) {
+                    self.searchContacts.append(contact)
+                }
+            }
+        }
+        self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        clearSearchContent()
+    }
+    
+    func clearSearchContent() {
+        // Clear the search bar text
+        searchBar.text=""
+        
+        // Close the virtual keyboard
+        searchBar.resignFirstResponder()
+        
+        // Reload data
+        self.searchContacts = self.contacts
+        self.tableView.reloadData()
+    }
+    
     @IBAction func selectAction(sender: UIBarButtonItem) {
         group = Group(name: self.textField.text!, photo: UIImage(asset:.Contacts_add_newmessage), contacts: groupContacts)
         for index in selectedData {
-            let contact = contacts[index]
+            let contact = searchContacts[index]
             group!.contacts.append(contact)
         }
         saveGroup()
