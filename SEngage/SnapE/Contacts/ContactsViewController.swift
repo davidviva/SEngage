349ce7240f27ffb656b571f980069329f9260000
davidviva <wooyanmis@gmail.com>
2016-05-27 13:38:20 -0400

    3.15.4
    detail button color add

--- SEngage/SnapE/Contacts/ContactsViewController.swift
+++ SEngage/SnapE/Contacts/ContactsViewController.swift
@@ -71,7 +71,6 @@ class ContactsViewController: UIViewController, UITableViewDelegate, UITableView
     func initNavigationBarItem() {
         let button: UIButton = UIButton(type: UIButtonType.Custom)
         button.setImage(UIImage(asset:.Barbuttonicon_addfriends), forState: .Normal)
-//        button.setTitle("➕", forState: .Normal)
         button.frame = CGRectMake(0, 0, 40, 30)
         button.imageView!.contentMode = .ScaleAspectFit;
         button.contentHorizontalAlignment = .Right
